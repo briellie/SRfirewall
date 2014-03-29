8533148fa30aa7514da3683305f2463743c0fbbf
bbruns@gmail.com <bbruns@gmail.com@e5899ace-8841-11de-95b9-8f387cfb8bc3>
2014-03-29 16:39:06 +0000

    More framework

---
+++ etc/ipv4/custom/nat.sh
@@ -0,0 +1,6 @@
+# This file is sourced by the main srfirewall program to inject
+# custom commands/rules during specific moments of the firewall
+# setup.
+#
+# In particular this file injects/commands rules:
+# Before the main nat rules are set up
