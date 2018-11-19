b19d4156d21904491b6934cb206d3a6ffbc05feb
Brielle Bruns <bruns@2mbit.com>
2018-11-19 10:50:00 -0700

    Add before/after run commands

---
+++ etc/ipv6/custom/runbefore.sh
@@ -0,0 +1,6 @@
+# This file is sourced by the main srfirewall program to inject
+# custom commands/rules during specific moments of the firewall
+# setup.
+#
+# In particular this file injects/commands rules:
+# Before all other things are done when the script loads
